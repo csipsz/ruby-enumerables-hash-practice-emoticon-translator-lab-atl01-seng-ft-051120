9ca63f912fedf7878c043e9cb8cdfc4e50de7547
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-04 23:46:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,19 @@
-# require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  emojis = YAML.load_file(file_path)
+  emojis.each do |name, values|
+      emojis[name] = {:english => values[0], :japanese => values[1]}
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  my_hash = load_library(file_path)
 end
 
-def get_english_meaning
-  # code goes here
+
+def get_japanese_emoticon(file_path, emoticon)
+  my_emoticons = load_library(file_path)
 end
+

de0ab2a2b74ab346a852dff0eab6c7f1ef6e95a4
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-05 00:30:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ def get_english_meaning(file_path, emoticon)
   my_hash = load_library(file_path)
   my_hash.each do |key, innerHash|
     if innerHash.has_value?(emoticon) == true
-      binding.pry
+      #binding.pry
       emoticon = key
     end
   end

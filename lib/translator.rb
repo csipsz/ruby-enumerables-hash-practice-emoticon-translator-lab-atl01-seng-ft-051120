393a40402badaa457c65b3b136c36a36a244ca8a
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-05 00:18:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,6 +16,7 @@ def get_english_meaning(file_path, emoticon)
      array.push(key, symbol, value)
       if array.include?(emoticon)
      return array[0]
+   end
    else 
      return "We don't have your emoji in our database"
   end

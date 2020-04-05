5784c10c03856a16f07d7ec963aa0a9a2a6bd29c
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-05 00:52:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_japanese_emoticon(file_path, emoticon)
   my_emojis = load_library(file_path)
   sorry_message = "Sorry, that emoticon was not found"
   my_emojis.each do |key, innerHash|
-    binding.pry
+    #binding.pry
     if innerHash[:english] == emoticon
       return innerHash[:japanese]
     end
